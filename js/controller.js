31f86bbe676cf272e0f0a5d6d76963b1524589cc
Yoni Pels <129108093+yonaPels@users.noreply.github.com>
2023-04-27 13:46:55 +0300

    Add files via upload

---
+++ js/controller.js
@@ -0,0 +1,165 @@
+'use strict'
+
+let gCanvas
+let gCtx
+let gLine = 0
+
+
+function onInit() {
+    renderGallery()
+}
+
+function renderGallery() {
+    const images = getImages()
+    let strHTML = images.map(img => `
+        <table>
+            <td><img src="img/${img.id}.jpg" onclick="onImgSelect('${img.id}')"> </td >
+        </table>
+    `).join('')
+    const elContainer = document.querySelector(".images-container")
+    elContainer.innerHTML = strHTML
+}
+
+onInit()
+
+
+function onImgSelect(id) {
+    setImg(id)
+    document.querySelector('.images-gallery').classList.add('hide')
+    document.querySelector('.editing-bord').classList.remove('hide')
+    document.querySelector('.create-meme').classList.remove('hide')
+    renderMeme()
+}
+
+function renderMeme() {
+    gCanvas = document.querySelector('#my-canvas')
+    gCtx = gCanvas.getContext('2d')
+    const meme = getMeme()
+    const { posX, posY } = meme.lines[gLine]
+    const elImg = new Image()
+    elImg.src = `img/${meme.selectedImgId}.jpg`
+    const url = meme.url
+    elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height)
+        drawText()
+        drawRect(posX, posY)
+    }
+}
+
+renderKeywords()
+function renderKeywords() {
+    const keywords = getKeywordSearchCountMap()
+    // console.log(Object.keys(keywords[0]));
+    const keys = Object.keys(keywords)
+
+    const strHTML = keys.map(key => `
+        <li style="font-size:${keywords[key] * 4}px" onclick="onSearchWord()">${key}</li>
+    `).join('')
+    // console.log(strHTML);
+    const elKeywordsBox = document.querySelector(".keywords-box")
+    elKeywordsBox.innerHTML = strHTML
+
+}
+
+function onInputText() {
+    setLineTxt(gLine)
+    renderMeme()
+}
+
+function onChangeLine() {
+    event.preventDefault()
+    const meme = getMeme()
+    if (meme.lines.length === 1) return
+    gLine = (gLine) ? 0 : 1
+    renderMeme()
+}
+
+function onAddLine() {
+    event.preventDefault()
+    addLine()
+    onChangeLine()
+    renderMeme()
+}
+
+function onDeletLine() {
+    event.preventDefault()
+    deletLine(gLine)
+    renderMeme()
+}
+
+function onSetFontSize(val) {
+    event.preventDefault()
+    setFontSize(gLine, val)
+    renderMeme()
+}
+
+function onSetAlignment(alignment) {
+    event.preventDefault()
+    setAlignment(gLine, alignment)
+    renderMeme()
+}
+
+function onSetFont(font) {
+    // event.preventDefault()
+    setFont(gLine, font)
+    renderMeme()
+}
+
+function onSetStrokeColor(color) {
+    // event.preventDefault()
+    setStrokeColor(gLine, color)
+    renderMeme()
+}
+
+function onSetColor(color) {
+    setColor(gLine, color)
+    renderMeme()
+}
+
+function downloadCanvas(elLink) {
+    // Protect the image soo attacker could not download imgs from diff domain
+    const data = gCanvas.toDataURL() // For security reason you cannot do toDataUrl on tainted canvas
+    // This protects users from having private data exposed by using images
+    // to pull information from remote web sites without permission.
+    elLink.href = data
+    elLink.download = 'my-img.jpg'
+}
+
+function canvasClicked(ev) {
+    const { offsetX, offsetY } = ev
+    console.log(ev);
+}
+
+function drawText(x, y) {
+    const { posX, posY, txt, size, align, color, strokeColor, font } =
+        getMeme().lines[gLine]
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = strokeColor
+    gCtx.fillStyle = color
+    gCtx.font = `${size}px ${font}`
+    // gCtx.textBaseline = 'middle'
+    gCtx.textAlign = align
+    gCtx.fillText(txt, posX + 250, posY) // Draws (fills) a given text at the given (x, y) position.
+    gCtx.strokeText(txt, posX + 250, posY) // Draws (strokes) a given text at the given (x, y) position.
+}
+
+function drawRect(x, y) {
+    // First way - drawing a rect by specifying a patch using the .rect() method,
+    // and then filling it with the .fill() method and stroking it with the .stroke() method
+    // gCtx.beginPath()
+    // gCtx.rect(x, y, 120, 120)
+    // gCtx.strokeStyle = 'black'
+    // gCtx.stroke()
+    // gCtx.fillStyle = 'orange'
+    // gCtx.fill()
+
+    // Second way - using the built in .fillRect() and .strokeRect() methods to directly
+    // paint on the canvas, without using a path
+    gCtx.strokeStyle = 'black'
+    gCtx.strokeRect(x, y - 30, 500, 60)
+    // gCtx.setFillColor('orange')
+    gCtx.fillStyle = "rgba(20,100, 250, 0.2)"
+    gCtx.fillRect(x, y - 30, 500, 60)
+}
+
+function onSearchWord() { console.log(this); }
---
+++ js/service.js
@@ -0,0 +1,86 @@
+'use strict'
+
+let gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+let gImgs = [
+    { id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] },
+    { id: 2, url: 'img/2.jpg', keywords: [] },
+    { id: 3, url: 'img/3.jpg', keywords: [] },
+    { id: 4, url: 'img/4.jpg', keywords: [] },
+    { id: 5, url: 'img/5.jpg', keywords: [] },
+    { id: 6, url: 'img/6.jpg', keywords: [] },
+    { id: 7, url: 'img/7.jpg', keywords: [] },
+    { id: 8, url: 'img/8.jpg', keywords: [] },
+    { id: 9, url: 'img/9.jpg', keywords: [] },
+    { id: 10, url: 'img/10.jpg', keywords: [] },
+    { id: 11, url: 'img/11.jpg', keywords: [] },
+    { id: 12, url: 'img/12.jpg', keywords: [] },
+    { id: 13, url: 'img/13.jpg', keywords: [] },
+    { id: 14, url: 'img/14.jpg', keywords: [] },
+    { id: 15, url: 'img/15.jpg', keywords: [] },
+    { id: 16, url: 'img/16.jpg', keywords: [] },
+    { id: 17, url: 'img/17.jpg', keywords: [] },
+    { id: 18, url: 'img/18.jpg', keywords: [] },
+];
+
+let gMeme = {
+    selectedImgId: 5, selectedLineIdx: 0, lines: [
+        { posX: 25, posY: 90, txt: 'I sometimes eat Falafel', size: 20, align: 'left', color: 'red', strokeColor: 'brown', font: 'impact' },
+    ]
+}
+
+function getImages() {
+    return gImgs
+}
+
+function getMeme() {
+    return gMeme
+}
+
+function getKeywordSearchCountMap() {
+    return gKeywordSearchCountMap
+}
+
+function setImg(id) {
+    gMeme.selectedImgId = id
+}
+
+function setLineTxt(line) {
+    gMeme.lines[line].txt = document.getElementById("text-input").value
+}
+
+function addLine() {
+    const line = { posX: 25, posY: 450, txt: 'Somthing else', size: 20, align: 'left', color: 'red', strokeColor: 'brown', font: 'impact' }
+    gMeme.lines.push(line)
+}
+
+function deletLine(line) {
+    gMeme.lines[line].remove()
+}
+
+function setFontSize(line, val) {
+    gMeme.lines[line].size += val
+}
+
+function setAlignment(line, alignment) {
+    gMeme.lines[line].align = alignment
+}
+
+function setFont(line, font) {
+    gMeme.lines[line].font = font
+}
+
+function setStrokeColor(line, clr) {
+    gMeme.lines[line].strokeColor = clr
+}
+
+function setColor(line, clr) {
+    gMeme.lines[line].color = clr
+}
+
+
+
+function getKeyWords() {
+    return gKeywordSearchCountMap
+}
+
+
