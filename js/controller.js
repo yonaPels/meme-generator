238404a9892158eae9421128ac0f907a5776414e
Yoni Pels <129108093+yonaPels@users.noreply.github.com>
2023-04-27 23:48:29 +0300

    Add files via upload

--- js/controller.js
+++ js/controller.js
@@ -52,7 +52,7 @@ function renderKeywords() {
     const keys = Object.keys(keywords)
 
     const strHTML = keys.map(key => `
-        <li style="font-size:${keywords[key] * 4}px" onclick="onSearchWord()">${key}</li>
+        <li style="font-size:${keywords[key] * 3}px" onclick="onSearchWord()">${key}</li>
     `).join('')
     // console.log(strHTML);
     const elKeywordsBox = document.querySelector(".keywords-box")
